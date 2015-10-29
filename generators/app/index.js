d3c062730fb488ca4d322aa5740aeaa6b909b44f
Brian Klepp <brian.klepp@thomsonreuters.com>
2015-10-29 12:24:04 -0500

    added new files

---
+++ generators/app/index.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+
+var yeoman = require('yeoman-generator');
+var pkg = require('../../package.json');
+
+module.exports = generators.Base.extend({
+
+    constructor: function () {
+        yeoman.generators.Base.apply(this, arguments);
+
+        // Define arguments
+        this.argument('appName', {
+            type: String,
+            required: false
+        });
+
+        this.version = pkg.version;
+
+    },
+    method1: function () {
+        console.log('method 1 just ran');
+    },
+    method2: function () {
+        console.log('method 2 just ran');
+    }
+
+});
